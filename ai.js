37bb45c5e71a2ee251b0f00a68c337fd3b9c5d3c
mirsulton699 <mirsultonmirsharifhonov5@gmail.com>
2026-08-02 23:27:42 +0500

    Update ai.js

--- ai.js
+++ ai.js
@@ -1,16 +1,18 @@
-async function sendMessage(){
+function sendMessage(){
 
     const input = document.getElementById("userInput");
     const chat = document.getElementById("chat");
 
-    const message = input.value.trim();
+    const text = input.value.trim();
 
-    if(!message) return;
+    if(text === ""){
+        return;
+    }
 
 
     chat.innerHTML += `
     <div class="card">
-        <p>👤 ${message}</p>
+        <p>👤 ${text}</p>
     </div>
     `;
 
@@ -18,67 +20,17 @@ async function sendMessage(){
     input.value = "";
 
 
-    chat.innerHTML += `
-    <div class="card" id="aiLoading">
-        <p>🤖 AI javob yozmoqda...</p>
-    </div>
-    `;
-
-
-    try {
-
-        const response = await fetch(
-            "https://api.openai.com/v1/chat/completions",
-            {
-
-            method:"POST",
-
-            headers:{
-                "Content-Type":"application/json",
-                "Authorization":"Bearer " + AI_CONFIG.apiKey
-            },
-
-
-            body:JSON.stringify({
-
-                model:"gpt-4o-mini",
-
-                messages:[
-                    {
-                        role:"user",
-                        content:message
-                    }
-                ]
-
-            })
-
-        });
-
-
-        const data = await response.json();
-
-
-        document.getElementById("aiLoading").remove();
-
+    setTimeout(()=>{
 
         chat.innerHTML += `
         <div class="card">
-            <p>🤖 ${data.choices[0].message.content}</p>
+            <p>
+            🤖 Infinity AI:
+            Hozircha test rejimida ishlayapman.
+            </p>
         </div>
         `;
 
-
-    } catch(error){
-
-        document.getElementById("aiLoading").remove();
-
-
-        chat.innerHTML += `
-        <div class="card">
-            <p>❌ AI ulanish xatosi</p>
-        </div>
-        `;
-
-    }
+    },800);
 
 }
